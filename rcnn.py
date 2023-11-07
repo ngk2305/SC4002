722273baa50e69d9ddd5fa3d7b099d36bf37602c
Khanh Nguyen <nguyengiakhanh2305@gmail.com>
2023-11-08 03:53:32 +0800

    train

--- rcnn.py
+++ rcnn.py
@@ -4,8 +4,8 @@ import torch.nn.functional as F
 
 
 class TextRCNN(nn.Module):
-    def __init__(self, sequence_length, num_classes, word_embedding_size, context_embedding_size,
-                 cell_type, hidden_size,  loss_function, l2_reg_lambda=0.0,):
+    def __init__(self, num_classes, word_embedding_size, context_embedding_size,
+                 cell_type,  loss_function, l2_reg_lambda=0.0,):
         super(TextRCNN, self).__init__()
         self.loss_fn = loss_function
         self.dropout = nn.Dropout()
@@ -15,35 +15,33 @@ class TextRCNN(nn.Module):
         self.bi_rnn = nn.RNN(input_size=word_embedding_size, hidden_size=context_embedding_size,
                              num_layers=1, bidirectional=True, batch_first=True)
         self.linear= nn.Linear(2*context_embedding_size,context_embedding_size)
-        self.word_rep = nn.Linear(2 * context_embedding_size + word_embedding_size, hidden_size)
-        self.max_pooling = nn.MaxPool1d(sequence_length)
-        self.output = nn.Linear(hidden_size, num_classes)
+        self.output = nn.Linear(2*context_embedding_size+word_embedding_size, num_classes)
 
     def forward(self, input_text, labels=None):
         rnn_out, _ = self.bi_rnn(input_text)
         rnn_out = self.linear(rnn_out)
-        print(rnn_out.size())
+        #print(rnn_out.size())
         c_left = torch.cat([torch.zeros_like(rnn_out[:, :1]), rnn_out[:, :-1]], dim=1)
         c_right = torch.cat([rnn_out[:, 1:], torch.zeros_like(rnn_out[:, :1])], dim=1)
-        print(c_left.size())
-        print(c_right.size())
-        x = torch.cat([c_left, input_text, c_right], dim=1)
-        print(x.size())
-        print(self.word_rep)
-        x = self.word_rep(x)
-        print(x.size())
-        x = x.permute(0, 2, 1)  # Adjust dimensions for max-pooling
-        print(x.size())
-        x = self.max_pooling(x)
-        print(x.size())
-        x = x.view(x.size(0), -1)  # Flatten
-        print(x.size())
+        #print(c_left.size())
+        #print(c_right.size())
+        x = torch.cat([c_left, input_text, c_right], dim=2)
+        #print(x.size())
+        x=x.permute(0,2,1)
+        x = F.max_pool1d(x, kernel_size= x.size(2))
+        x = x.permute(0, 2, 1)
+        #print(x.size())
+
         logits = self.output(x)
+        logits= logits.view(-1)
         if labels is not None:
             loss = self.loss_fn
             return loss
+        print(logits.size())
         output = torch.argmax(logits, dim=-1)
-        return logits, output
+
+        print(output)
+        return output
 
 
 
--- train.py
+++ train.py
@@ -1,26 +1,121 @@
 import torch
 import pandas as pd
 from rcnn import TextRCNN
+from torch.utils.data import DataLoader, random_split
 import ast
+from data_helpers import MyDataset
+from data_helpers import EarlyStopper
+from tqdm import tqdm
+from torch import optim
+import numpy as np
+from sklearn.metrics import f1_score, precision_score, recall_score, confusion_matrix
 
-sequence_length = 100  # Change to your desired sequence length
 num_classes = 13  # Change to your number of classes
 word_embedding_size = 50  # Change to your desired embedding size
 context_embedding_size = 50  # Change to your desired context embedding size
 cell_type = "lstm"  # Change to "vanilla" or "gru" if needed
-hidden_size = 256  # Change to your desired hidden size
-loss_fn= torch.nn.CrossEntropyLoss
+loss_fn= torch.nn.CrossEntropyLoss()
 batch_size=1
 
-model = TextRCNN(sequence_length, num_classes, word_embedding_size, context_embedding_size, cell_type,
-                 hidden_size,loss_fn)
 
+#read/load data
 df = pd.read_csv('selected_data.csv')
-df['word_embeddings'] = df['word_embeddings'].apply(ast.literal_eval)
-input_text= torch.Tensor(df.loc[0, 'word_embeddings'])
-print(input_text)
-input_text.size()
+print('Done read csv file!')
+df['word_embeddings']= df['word_embeddings'].apply(ast.literal_eval)
+print('Done converting WEm into literals')
+df['word_embeddings']= df['word_embeddings'].apply(torch.Tensor)
+print('Done converting WEm into tensors')
+my_dataset = MyDataset(df)
 
+#train,validation,test split param
+validation_ratio = 0.2
+test_ratio = 0.2
 
-logits, output = model(input_text)
-print(output)
+#Splitting data
+validation_size = int(validation_ratio * len(my_dataset))
+test_size = int(test_ratio * len(my_dataset))
+train_size = len(my_dataset) - validation_size - test_size
+train_dataset, validation_dataset, test_dataset = random_split(
+    my_dataset, [train_size, validation_size, test_size])
+
+# Create data loaders for training, validation, and testing
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+validation_loader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+# Initialize variables for early stopping
+early_stopper = EarlyStopper(patience=3)
+model = TextRCNN( num_classes, word_embedding_size, context_embedding_size, cell_type,
+                 loss_fn)
+optimizer= optim.Adam(model.parameters(), lr=0.001)
+
+EPOCHS = 100
+torch.set_grad_enabled(True) 
+train_accuracy = []
+train_loss = []
+
+test_accuracy = []
+test_loss = []
+
+for epoch in tqdm(range(EPOCHS), desc="Training Progress"):
+    model.train()
+
+    train_loss_in_epoch = []
+    train_accuracy_in_epoch = []
+
+    for batch_x, batch_y in train_loader:
+        # forward pass
+        outputs = model(batch_x).unsqueeze(0)
+        outputs = outputs.type(torch.float)
+        print(outputs)
+        print(outputs.size())
+
+        batch_y= batch_y.type(torch.float)
+        print(batch_y)
+        print(batch_y.size())
+
+        loss = loss_fn(outputs, batch_y)
+        train_loss_in_epoch.append(loss)
+
+        acc = (outputs.round() == batch_y).float().mean()
+        train_accuracy_in_epoch.append(acc)
+        # backward pass
+        optimizer.zero_grad()
+        loss.backward()
+
+        # update weights
+        optimizer.step()
+
+    train_loss.append(torch.mean(torch.stack(train_loss_in_epoch)).item())
+    train_accuracy.append(np.mean(train_accuracy_in_epoch))
+
+    model.eval()
+    eval_loss_in_epoch = []
+    eval_accuracy_in_epoch = []
+
+    with torch.no_grad():
+        for batch_x, batch_y in tqdm(
+                test_loader,
+                desc="Epoch {} Testing".format(epoch),
+                leave=False):
+            # Calculating loss
+            predicted_labels = model.forward(batch_x).squeeze()
+
+            loss = loss_fn(predicted_labels, batch_y)
+            eval_loss_in_epoch.append(loss)
+
+            accuracy = precision_score((predicted_labels > 0.5).float(), batch_y)
+            eval_accuracy_in_epoch.append(accuracy)
+
+    test_loss.append(torch.mean(torch.stack(eval_loss_in_epoch)).item())
+    test_accuracy.append(np.mean(eval_accuracy_in_epoch))
+
+    # Early Stopping
+    if early_stopper.early_stop(
+            torch.mean(torch.stack(eval_loss_in_epoch)).item()):
+        print(f"Early Stop at Epoch {epoch + 1}!")
+        break
+
+print(
+    f"Epoch {epoch + 1} | Train Loss {train_loss[-1]:.5f} | Train Acc {train_accuracy[-1]:.5f} | Test Loss {test_loss[-1]:.5f} | Test Acc {test_accuracy[-1]:.5f}"
+)
